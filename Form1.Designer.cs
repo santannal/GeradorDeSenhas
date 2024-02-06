c7081128553fdf9a2215063922a49d01e3a675c5
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-02-06 10:48:26 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace GeradorDeSenhas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableExibirSenhas = new System.Windows.Forms.ListBox();
+            this.numeroCaracteres = new System.Windows.Forms.NumericUpDown();
+            this.lblCarac = new System.Windows.Forms.Label();
+            this.cMaiusculos = new System.Windows.Forms.CheckBox();
+            this.cEspeciais = new System.Windows.Forms.CheckBox();
+            this.cAlgarismos = new System.Windows.Forms.CheckBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numeroCaracteres)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableExibirSenhas
+            // 
+            this.tableExibirSenhas.FormattingEnabled = true;
+            resources.ApplyResources(this.tableExibirSenhas, "tableExibirSenhas");
+            this.tableExibirSenhas.Name = "tableExibirSenhas";
+            // 
+            // numeroCaracteres
+            // 
+            resources.ApplyResources(this.numeroCaracteres, "numeroCaracteres");
+            this.numeroCaracteres.Name = "numeroCaracteres";
+            // 
+            // lblCarac
+            // 
+            resources.ApplyResources(this.lblCarac, "lblCarac");
+            this.lblCarac.Name = "lblCarac";
+            // 
+            // cMaiusculos
+            // 
+            resources.ApplyResources(this.cMaiusculos, "cMaiusculos");
+            this.cMaiusculos.Name = "cMaiusculos";
+            this.cMaiusculos.UseVisualStyleBackColor = true;
+            // 
+            // cEspeciais
+            // 
+            resources.ApplyResources(this.cEspeciais, "cEspeciais");
+            this.cEspeciais.Name = "cEspeciais";
+            this.cEspeciais.UseVisualStyleBackColor = true;
+            // 
+            // cAlgarismos
+            // 
+            resources.ApplyResources(this.cAlgarismos, "cAlgarismos");
+            this.cAlgarismos.Name = "cAlgarismos";
+            this.cAlgarismos.UseVisualStyleBackColor = true;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btnGerar, "btnGerar");
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.UseVisualStyleBackColor = false;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.RosyBrown;
+            resources.ApplyResources(this.btnSair, "btnSair");
+            this.btnSair.Name = "btnSair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.cAlgarismos);
+            this.Controls.Add(this.cEspeciais);
+            this.Controls.Add(this.cMaiusculos);
+            this.Controls.Add(this.lblCarac);
+            this.Controls.Add(this.numeroCaracteres);
+            this.Controls.Add(this.tableExibirSenhas);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numeroCaracteres)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox tableExibirSenhas;
+        private System.Windows.Forms.NumericUpDown numeroCaracteres;
+        private System.Windows.Forms.Label lblCarac;
+        private System.Windows.Forms.CheckBox cMaiusculos;
+        private System.Windows.Forms.CheckBox cEspeciais;
+        private System.Windows.Forms.CheckBox cAlgarismos;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorDeSenhas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            // Define uma string inicial com os caracteres minúsculos do alfabeto
+            string caracteres = "abcdefghijklmnopqrstuvwxyz";
+
+            // Adiciona números à string de caracteres se a opção estiver marcada
+            if (cAlgarismos.Checked)
+            {
+                caracteres += "0123456789";
+            }
+
+            // Adiciona caracteres especiais à string de caracteres se a opção estiver marcada
+            if (cEspeciais.Checked)
+            {
+                caracteres += "!@#$%&*()-_+={}[];.,:";
+            }
+
+            // Adiciona letras maiúsculas à string de caracteres se a opção estiver marcada
+            if (cMaiusculos.Checked)
+            {
+                caracteres += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
+
+            // Limpa os itens da tabela onde as senhas serão exibidas
+            tableExibirSenhas.Items.Clear();
+
+            // Inicia um objeto Random para sorteio
+            Random sorteio = new Random();
+
+            // Define a quantidade de senhas a serem geradas (atualmente, sempre 1)
+            int qtd = 1;
+
+            // Loop para gerar a quantidade de senhas especificada
+            for (int num = 1; num <= qtd; num++)
+            {
+                // Inicia uma StringBuilder para construir a senha
+                StringBuilder senha = new StringBuilder();
+
+                // Loop para construir a senha com base no número de caracteres especificado
+                for (int x = 1; x <= numeroCaracteres.Value; x++)
+                {
+                    // Gera um índice aleatório para escolher um caractere da string de caracteres
+                    int local = sorteio.Next(0, caracteres.Length);
+
+                    // Adiciona o caractere escolhido à senha
+                    senha.Append(caracteres[local].ToString());
+                }
+
+                // Adiciona a senha à tabela de exibição
+                tableExibirSenhas.Items.Add(senha.ToString());
+            }
+        }
+
+        // Método para fechar a aplicação
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
+
